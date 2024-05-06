945cd6d26fc10ffe9561feb4b43b5e0adfde7ebc
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-05-06 11:51:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
+
+  try{
+    let existingContent =await fs.readFile(fileName,'utf8');
+    let updateContent = existingContent +fileContent;
+    await fs.writeFile(fileName, updateContent,'utf8');
+    return updatedContent;
   // write code here
   // dont change function name
-  
+  }catch(error){
+    console.error("Error updating file:",error);
+  }
 };
 
 module.exports = updateFile;
